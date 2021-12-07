8231013555ca2cbbd7648dcbe28da588f51c9a2e
mayuxin <mayuxin@people.cn>
2021-12-07 18:11:13 +0800

    add about

---
+++ about/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: mayuxin
+ * @Date:   2017-12-01 11:01:40
+ * @Last Modified by:   mayuxin
+ * @LastModified time: 2018-12-11 10:35:14
+ */
+
+$(document).ready(function() {
+    
+})
+
---
+++ news/index.js
@@ -0,0 +1,274 @@
+/*
+ * @Author: mayuxin
+ * @Date:   2017-12-01 11:01:40
+ * @Last Modified by:   mayuxin
+ * @LastModified time: 2018-12-11 10:35:14
+ */
+var testdata = {
+		"id": "1468148060517638146",
+		"sysCode": "article",
+		"articleId": "1468148060517638146",
+		"type": "common",
+		"categoryId": 344,
+		"realCategoryId": 344,
+		"title": "科技护肤+口腔健康，碧捷如何抓住两条黄金赛道",
+		"listTitle": "科技护肤+口腔健康，碧捷如何抓住两条黄金赛道",
+		"source": "消费者报道",
+		"description": "2016年，碧捷相继成为日本美容仪品牌雅萌Ya-man和美国冲牙器品牌Waterpik洁碧的中国总代理，并以这两款个护小家电产品做为切口，在“科技护肤“、“口腔健康”两大领域进行纵向深耕，陆续成为护肤品牌RevisionSkincare和PCASkin，牙膏品牌倍洁丽PearlDrops牙膏和Toms在中国的总代理，截止于2021年，碧捷已经成为7个国际品牌的中国独家总代理。",
+		"likes": 0,
+		"hits": 0,
+		"comments": 0,
+		"tags": "品牌,美容,冲牙器,市场,消费者,口腔,小家电,需求,日本,功效",
+		"content": "<div><p><span>据国家统计局数据显示，近10年全国居民人均可支配收入累计实际增长100.8%。当前居民消费力增长与互联网快速发展并行，以及以Z世代为代表的追逐个性潮流、生活品质、关注颜值的新一代消费群体的成长，助力了“颜值经济”的蓬勃发展。“我们做‘脖子以上’的生意”，广东碧捷洁净科技有限公司（以下简称“碧捷”）董事长王振刚曾在接受媒体采访时表示，中国消费升级，新一代年轻女性更愿意为美丽“投资”。</span></p><p><span>据介绍，碧捷成立于2013年，是一家聚焦“科技护肤“、“口腔健康”两大领域，为中国家庭带来全球创新黑科技产品的全链路新零售公司。2016年，碧捷相继成为日本美容仪品牌雅萌Ya-man 和美国冲牙器品牌Waterpik 洁碧的中国总代理，并以这两款个护小家电产品做为切口，在“科技护肤“、“口腔健康”两大领域进行纵向深耕，陆续成为护肤品牌Revision Skincare和PCA Skin，牙膏品牌倍洁丽Pearl Drops牙膏和Tom’s在中国的总代理，截止于2021年，碧捷已经成为7个国际品牌的中国独家总代理。</span></p><div><img src=\"http://luckyimgs.peopletech.cn/image/20211207/theone3_1_202006c5d0e3b5-341f-49e7-8bea-ee29592956ff.jpg\" width=\"640\" height=\"425\"/></div><p><span>在帮助海外品牌打开中国市场的同时，碧捷始终聚焦于提高自身的产品研发能力、渠道建设能力和服务体验升级。从2018年开始，碧捷就在不断总结过去积累的多方面能力和经验赋能到自有品牌建设上，推出口腔护理品牌歌露白，2020年首款产品主打科技香氛的漱口水正式推出。</span></p><h1>定制化产品策略，开拓家用美容仪、冲牙器中国市场</h1><p><span>在消费升级大环境和“颜值经济”的加持下，“全身颜值都能’打’，悦己悦人悦世界”已经成为新一代消费者的生活方式 ，消费者对颜值的追求已经从“面部美”延伸到“口腔美”、“形体美”等全方位的升级。</span></p><p><span>在此背景下，个护小家电市场也从原来简单的电动剃须刀、电吹风发展为美发、美齿、美容、美体“四美”并行的局面，其中美容、美齿类产品凭借轻医美、操作简单等优势，让消费者即使是在家里也能拥有“变美”的条件，脱颖而出成为消费新宠。据GfK数据显示，中国个护小家电七大品类（电吹风、剃须刀、电动牙刷、美容仪、美发器、女士脱毛仪、冲牙器）市场规模从2015年的130亿元到2019年增长至416亿元，5年复合增长率达到27%。即便是疫情期间，数据显示，2020年前9个月个护小家电零售额也达到343亿元，同比增长17%。</span></p><p><span>时间倒回到2015年，在各大偶像、都市电视剧的热播下，“初老\"的概念开始萌生，都市白领的容貌焦虑开始蔓延，微整形成为新的潮流，且收入越高的消费者越青睐微整形和面部年轻化（抗衰老），医美行业开始受到资本市场的密切关注。而各类功效不同的家用美容仪，恰恰可以填补消费者在医美后的后续需求，能帮助维持医美效果，也有助于皮肤的日常保养。</span></p><p><span>但当时，中国的家用美容仪市场主要是主打洁面仪器，基于这样的市场需求和市场洞察下，2015年9月，碧捷成为日本最大美容仪企业雅萌在中国的总代理 。</span></p><p><span>据介绍，雅萌最初进入中国市场的是其在日本的王牌产品多功能美容仪，但碧捷在做该产品的推广过程中发现，中日两国的家用美容仪消费群体、对产品的功能需求、使用习惯等方面都存在很大的差异，比如日本的主流消费群体是35岁以上的已婚女性为主，但中国是年龄在25岁以上的职业女性；日本消费者可以做完15分钟全流程，中国消费者诉求是快速、有效……尽管雅萌美容仪在日本已有42年的品牌历史，市占率稳居第一，但不适合中国的消费需求。</span></p><p><span>于是，碧捷与雅萌团队沟通，研发一款针对中国女性的消费偏好和肤质特征的单一功能的美容仪产品。目前雅萌热销的ACE四环射频仪已经是升级的第三代产品，该款美容仪主打提拉紧致、淡化皱纹功效，全脸使用时间只要6 分钟；此外，雅萌对多功能美容仪产品MAX进行了升级迭代，将17分钟的护肤程序缩短至14分钟。</span></p><p><span>除了定制化产品外，碧捷还根据中国消费者购物习惯提供定制化的服务，比如7天无理由退货等。除了线上渠道，线下下体验也同样值得重视，美容仪作为美容护肤需求的延伸，体验是消费者购买前的重要需求，目前，碧捷在北京、上海、杭州、广州等城市开设 400 +家线下终端，以及线下直营旗舰店，实现线上和线下一体化的新零售，抓好线下渠道是进一步提升品牌力的重要方式，基于以上，雅萌连续四年蝉联天猫双十一美容仪器销量冠军。</span></p><p><span>而碧捷代理的另一款个护小家电产品冲牙器则是碧捷董事长王振刚在纠正牙齿的过程中发现中国消费者对冲牙器的需求，于是2016年，碧捷成为冲牙器发明者美国洁碧（WaterPik）公司在中国的总代理。5年时间里，伴随着消费人群的迭代，“90后”的许多消费新习惯也在不断涌现，特别是口腔护理意识提升带动了专业护理产品的普及，冲牙器正在进入更多消费者的视野，其潜在需求正在持续规模性释放。“颜值经济已经武装到牙齿了，口腔革命正在来袭\"。</span></p><p><span>从雅萌、洁碧两款产品可以看出，碧捷在选择代理的品牌和产品过程中有着自己的标准，即安全有效、健康美观。“整合全球资源，为中国消费者引入海外优秀品牌，成就中国消费者的美丽和自信是碧捷公司的使命\"。</span></p><h1>坚持技术创新、产品安全，推动行业标准化</h1><p><span>无论家用美容仪还是冲牙器，都是在经济持续增长、消费不断升级、生活品质提升、个人意识觉醒的中国消费市场第四次浪潮下，多元化消费需求驱动的新生力量，都有着旺盛的“生命力”。</span></p><p><span>巨大的行业发展潜力，资本的推动，吸引了众多企业和品牌入局。主要分为三类：一是在此领域布局多年的海外品牌纷纷进入中国市场；二是传统的个护企业、家电企业进行产品线扩充、升级、入局；三是新兴的国产品牌。据企查查的数据显示，目前我国美容仪器相关注册企业约8万余家，仅在2019年便新增注册企业2.4万家；包括小米生态链旗下的品牌、美的等传统家电品牌等在内中国冲牙器行业的企业约有200家。</span></p><p><span>作为美容护肤、口腔健康衍生出来的细分需求，相比其它家电，安全问题更受消费者关心，当产品拥有一定的技术优势，并可以证明产品安全性时，消费者也会相应给予更多的信任。但目前，美容仪、冲牙器行业海外品牌占据半壁江山，另外约50%的市场份额非常零碎，有很多长尾的中小品牌，引发了一些鱼龙混杂、虚假宣传、标准体系有待完善等问题亟需解决。</span></p><p><span>过去两年，碧捷投入数百万元打造内部实验室Z—Lab，前置性检验产品的安全性和功效性。据介绍，Z—Lab是一个覆盖美健个护产品全链路的一流实验室，并 根据CNAS-CL01：2018《检测盒校准实验室能力认可准则》要求，持续改善体系流程。目前，该实验室一期已经投入数百万，目前已有数十人的专业检测技术团队，具备美容仪、冲牙器的安规、性能、可靠性等多项检测能力本着以用户安全为基础，致力于提高用户体验和产品可靠性为核心，展开各项测试工作。</span></p><p><span>除了加强自身检测能力建设外，碧捷自2015年把雅萌带入中国市场以来，就与多家公立三甲医院合作，开展人体功效性临床测试。据了解，雅萌是国内首家与三甲医院进行合作的美容仪品牌，截止2020年，雅萌全系列产品人体功效和安全性试验已完成近千样本，在功效和安全方面已经积累了丰富的数据资源。</span></p><p><span>如今，碧捷又率先推动家用美容仪、冲牙器行业标准的建立，希望能够兼顾产品的安全性和功效性。2020年，碧捷联合威凯检测技术有限公司、天猫平台、中山大学附属第三医院、四川大学华西医院等成立工作组，共同参与家用美容仪行业团体标准的制定。此外，碧捷还参与了《电动冲牙器》(T/CHEAA 0014-2020)团体标准；《家用和类似用途皮肤美容器评价规范》团体标准；《家用和类似用途美眼仪》团体标准……等一系列行业标准的建立。</span></p><p><span>碧捷一直以对用户负责的态度，坚持对产品品质高标准、严要求，致力于产品的安全性和功效性研究，专注于提高自身“产品力”，同时也希望将这些能力赋能给行业 推动行业实现健康和可持续发展，为合作伙伴、行业和利益相关者打造美好、和谐、共赢的环境。</span></p><h1>横向拓品+纵向深耕，建立”护城河“</h1><p><span>可预见的是，中国美容仪市场和口腔护理市场是一个百亿、千亿规模的市场，各企业要如何紧跟着市场发展的脚步，保有一席之地呢？碧捷给出的答案是不断提高\"产品创新\"和\"渠道创新\"这2大能力。</span></p><p><span>在产品创新上，一方面根据新用户的诉求和新消费场景，不断提高技术储备，打造功效和安全于一体的王牌产品，并持续进行功能创新、技术差异化、场景仪式感打造；此外，将前沿护肤、护牙成果进行赋能的美容仪、冲牙器等品类是未来的趋势；在渠道创新上一方面提高用户运营能力，另一方面与时俱进，流量在哪里，消费者在哪里，就去哪里。</span></p><p><span>但小家电产品最终决胜点还是供应链，比如目前美容仪的供应链散布在日本、美国、欧洲和中国，但未来随着中国供应链本土化进程的推进，国产美容小家电的品质也将逐步提升。“碧捷与雅萌未来不排除共同在中国市场推出更本土化的创新美容小家电产品。”</span></p><p><span>据介绍，碧捷聚焦“科技护肤“、“口腔健康”两大领域，品牌分为代理的国际品牌和自主研发的自有品牌两类，共有2条产品线：一是美容仪、冲牙器为代表的的“仪器类”小家电产品，另一条是以护肤品、牙膏和漱口水为代表的“个护类”产品。</span></p><p><span>由于美容仪和冲牙器是具有小家电和个人护理双重属性的产物，并且在中国是一个新兴的品类，碧捷便以此作为进入“护肤“和“口腔”两大个护领域“敲门砖”，这是一个要比小家电市场大10倍以上的市场。据安信证券报告显示，2019年中国个人护理市场规模增长至4587亿元，远超2015年的3058亿元，复合年增长率10.7%。预计2024年，市场规模将增长至7014亿元，复合年增长率为8.9%。</span></p><p><span>”2018年，碧捷决定将过去积累的科技创新能力、数字化品牌营销能力、追求极致用户体验的售后服务能力赋能到自有品牌建设上。2020年8月，碧捷首个自有品牌“歌露白”正式进入市场，推出首款产品主打氨基酸的香氛漱口水。碧捷方面表示，首个自有品牌选中口腔健康下漱口水这一细分赛道通过观察发现中国正处于口腔健康消费升级阶段，而口腔清洁是市场增长的主要动力。未来，碧捷将推出更多更优质的自有国货品牌。</span></p><p><span>从轻资产的品牌代理，到重资产的自研品牌；从小家电行业切入，到在个护赛道里深根；将美容仪、冲牙器等畅销品和牙膏、漱口水等短销品互补、线上线下相结合的运营策略，碧捷正在通过一系列组合拳搭建自己的“护城河”。</span></p></div><p>来源：消费者报道</p>",
+		"viewType": "normal",
+		"shareUrl": "https://theone3-honor.peopletech.cn/h5/detail/normal/1468148060517638146",
+		"isTailor": true,
+		"date": "2021-12-07T17:09:47+0800",
+		"isReference": false,
+		"firstCategoryId": "13",
+		"images": ["http://luckyimgs.peopletech.cn/image/20211207/theone3_1_202006f7918d0f-1039-4e63-acb6-05716228eb12.jpg"],
+		"categoryInfo": {
+			"id": 344,
+			"name": "科技",
+			"parentId": 333,
+			"recommend": "0"
+		},
+		"outId": "fefe2fb0a3bf46c5b4f80b4f7c61c371"
+	}
+
+// var playState = 0; //0初始 1播放 2暂停
+
+$(document).ready(function() {
+    renderReady();
+    renderHtml(testdata);
+})
+
+function renderHtml(json) {
+    renderHeader(json);
+    // renderImage(json);
+    renderContent(json);
+    bindEvent();
+    renderHtmlFinish(json);
+}
+/**
+ * 渲染头部
+ */
+function renderHeader(json) {
+    if (json.authors) {
+        json.authors = json.authors.replace(/<br>/g, " ");
+        json.authors = json.authors.replace(/<br\/>/g, " ");
+    }
+    var header = template("tmpHeader", json);
+    $("#htmlHeader").html(header);
+    if (!json.introTitle) {
+        $('sho_title').hide();
+    }
+    if (!json.subTitle) {
+        $('sub_title').hide();
+    }
+    if (json.authors) {
+        $(".authors").show();
+    }
+}
+/**
+ * 渲染图片
+ */
+function renderImage(json) {
+    if (json.imgall != " " && json.imgall.length > 0) {
+        for (var i = 0; i < json.imgall.length; i++) {
+            var img = template('tmpImg', json.imgall[i]);
+            $("#htmlImage").append(img);
+        }
+
+    }
+
+}
+/**
+ * 渲染正文
+ */
+function renderContent(json) {
+    $("#htmlDetail").html(json.content + '<div class="allTip"><p>- 已显示全部内容 -</p></div>');
+}
+/**
+ *渲染视频
+ */
+function renderMedia(json) {
+    if (json.type == "video" && json.medias != " " && json.medias.length > 0) {
+        var htmlMedia = $("#htmlMedia");
+        htmlMedia.show();
+        var w = htmlMedia.css("width");
+        var h = parseFloat(w) * 9 / 16 + "px";
+        htmlMedia.css("height", h);
+        // var x=htmlMedia.offset().left*window.devicePixelRatio;
+        // var y=htmlMedia.offset().top*window.devicePixelRatio;
+        // var h=htmlMedia.height()*window.devicePixelRatio;
+        // var w=htmlMedia.width()*window.devicePixelRatio;
+        // log("x="+x+",y="+y+",w="+w+",h="+h+"scale="+window.devicePixelRatio);
+        // var tmp='{"x":'+x+","+'"y":'+y+","+'"w":'+w+","+'"h":'+h+'}'
+        var json = {};
+        json.x = htmlMedia.offset().left * window.devicePixelRatio;
+        json.y = htmlMedia.offset().top * window.devicePixelRatio;
+        json.h = htmlMedia.height() * window.devicePixelRatio;
+        json.w = htmlMedia.width() * window.devicePixelRatio;
+        addMedia(JSON.stringify(json));
+    }
+}
+/**
+ *事件绑定
+ */
+function bindEvent() {
+    $("body").unbind('click').on("click", '#content img', function(e) {
+        var index = $("#content img").index(this);
+        var url = new Array();
+        $("#content img").each(function(i) {
+            url[i] = '"' + $(this).attr("src") + '"';
+        });
+        var json = '{"position":' + index + ',"urls":[' + url + ']}';
+        checkBigPic(json);
+    });
+    $('#content img').error(function() {
+        $(this).attr('src', "css/img/pic_default_common.png");
+    });
+    $('.playBtn').unbind('click').on("click", function(e) {
+        handlerVoice();
+    });
+    $('.stopBtn').unbind('click').on("click", function(e) {
+        stopVoiceUi();
+        stopVoice();
+    });
+}
+/**
+ * 渲染结束
+ */
+function renderHtmlFinish(json) {
+    $('#loading').css('visibility', 'hidden');
+    if (json.content) {
+        $('#readlay').css('visibility', 'visible');
+    }
+    renderFinish();
+}
+/**
+ * 处理播放按钮ui   
+ */
+function handlerVoiceUI(state) {
+    if (state == 0) {
+        stopVoiceUi();
+    } else if (state == 1) {
+        $('.playBtn').attr('src', "css/img/pic_audio_pause.gif");
+        $('.stopBtn').show();
+    } else if (state == 2) {
+        $('.playBtn').attr('src', "css/img/pic_audio_play.png");
+    }
+}
+
+/**
+ * 停止播放
+ */
+function stopVoiceUi() {
+    $('.stopBtn').hide();
+    $('.playBtn').attr('src', "css/img/pic_audio_normal.png");
+}
+
+function setFontSize(size) {
+    $("#htmlDetail").css("font-size", getFontSizeClass(size));
+}
+
+function getFontSizeClass(size) {
+    if (size == 1) {
+        return "0.12rem";
+    } else if (size == 2) {
+        return "0.14rem";
+    } else if (size == 3) {
+        return "0.16rem";
+    } else if (size == 4) {
+        return "0.18rem";
+    } else if (size == 5) {
+        return "0.2rem";
+    } else {
+        return "2.66em";
+
+    }
+}
+
+/*****************native funtion**************/
+
+/**
+ * 准备发起网络请求
+ */
+function renderReady() {
+    invoke("renderReady", "{}");
+}
+/**
+ * 页面加载完成
+ */
+function renderFinish() {
+    invoke("renderFinish", "{}");
+}
+
+/**
+ * 查看大图
+ */
+function checkBigPic(index) {
+    invoke("checkBigPic", index);
+}
+/**
+ * 添加播放器
+ */
+function addMedia(json) {
+    invoke("addMedia", json);
+}
+/**
+ * 从暂停状态开始语音播报
+ */
+function playVoice() {
+    invoke("playVoice", "{}");
+}
+/**
+ * 开始语音播报
+ */
+function startVoice() {
+    invoke("startVoice", "{}");
+}
+/**
+ * 暂停语音播报
+ */
+function pauseVoice() {
+    invoke("pauseVoice", "{}");
+}
+/**
+ *停止语音播报
+ */
+function stopVoice() {
+    invoke("stopVoice", "{}");
+}
+
+function handlerVoice() {
+    invoke("handlerVoice", "{}");
+}
+/**
+ * log 输出
+ */
+function log(str) {
+    var log = '{"log":' + str + '}'
+    invoke("log", log);
+
+}
+
+function invoke(func, json) {
+    var fjson = '{"func":"' + func + '","data":' + json + '}';
+    // if(isAndroid){
+    //  window.jsobj.invoke(func,json);
+    //     // window.jsobj[func](json);
+    // }
+    if (window.jsobj && window.jsobj.invoke) {
+            window.jsobj.invoke(fjson);
+    }
+    // if(isIOS){
+    //  window.webkit.messageHandlers.invoke.postMessage(func,json);
+    //  // window.webkit.messageHandlers[func].postMessage(json);
+    // }
+    //鸿蒙
+    if (window.jsobj && window.jsobj.call) {
+        window.jsobj.call(fjson);
+    }
+}
